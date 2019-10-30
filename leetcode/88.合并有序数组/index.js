8f1b2afd9ea005678b3fb8275ed812fc58a5e162
timelessover <shanpaodaxia@qq.com>
2019-10-30 22:06:52 +0800

    '合拼有序数组'

--- leetcode/88.合并有序数组/index.js
+++ leetcode/88.合并有序数组/index.js
@@ -6,22 +6,13 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-    // 设置两个数组的指针
-    let p1 = m - 1
-    let p2 = n - 1
-    // 设置 m1 指针
-    let p = m + n - 1
-    if ((p1 < 0) && (p2 >= 0)) {
-        return nums2
+    let pos = m + n - 1;
+    while (n > 0) {
+        if (m > 0 && nums1[m - 1] > nums2[n - 1]) {
+            nums1[pos--] = nums1[--m];
+        } else {
+            nums1[pos--] = nums2[--n];
+        }
     }
-    if ((p1 >= 0) && (p2 < 0)) {
-        return nums1
-    }
-    // 双指针，从后往前
-    while ((p1 >= 0) && (p2 >= 0)) {
-        nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
-    }
-
-    return nums1
 };
 
