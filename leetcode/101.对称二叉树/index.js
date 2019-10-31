e1a31d73e46d2f4363b26915969c5ccfe416e58a
Chrisping <34708197+timelessover@users.noreply.github.com>
2019-10-31 15:30:43 +0800

    Update index.js

--- leetcode/101.对称二叉树/index.js
+++ leetcode/101.对称二叉树/index.js
@@ -1,13 +1,10 @@
-var isSymmetric = function (root) {
-    if (!root) return true
-    var Symmetric = function (left, right) {
-        if (left == null && right !== null || left !== null && right == null) {
-            return false
-        }
-        if (left == null && right == null) {
-            return true
-        }
-        return left.left == left.right && Symmetric(left.left, right.right) && Symmetric(right.right, left.left)
+var isSymmetric = function(root){
+    if(!root) return true
+    var _isSymmetric = function(t1,t2){
+        if(t1 === null && t2 !== null || t1 !== null && t2 === null) return false
+        if(t1 === null && t2 === null) return true
+        if(t1.val !== t2.val) return false
+        return _isSymmetric(t1.left,t2.right) && _isSymmetric(t2.left,t1.right)
     }
-    return Symmetric(root.left, root.right)
-};
+    return _isSymmetric(root.left,root.right)
+}
