637d99615a2b79ad51a9cdc2dc92db42da540dc8
YanPing Liu <yanping.liu@questmobile.com.cn>
2019-10-31 16:53:28 +0800

    leetcode

--- leetcode/124.二叉树最大路径总和/index.js
+++ leetcode/124.二叉树最大路径总和/index.js
@@ -1,24 +1,23 @@
-var maxPathSum = function (root) {
-    if (root == null) return 0;
-    // 记录最大值
-    let max = 0
-    // 从第一个开始递归
-    dp(root)
-    function dp(root) {
-        // 左右树的最长路径总和
-        let leftval = dp(root.left);
-        let rightval = dp(root.right);
-        let currentMax = root.val;
-        if (leftval > 0) {
-            currentMax += leftval;
-        }
-        if (rightval > 0) {
-            currentMax += rightval;
-        }
-        if (currentMax > max) {
-            max = currentMax;  // 更新全局最大值
-        }
+// db
 
-        return Math.max(root.val, Math.max(leftval + root.val, rightval + root.val)); // 返回经过root为根节点的最大路径值
+var maxPathSum = function (root) {
+    // 记录最大值
+    let max = 0;
+    // 处理根节点小于0的情况
+    if (root.val <= 0) {
+        max = root.val
     }
+    const dp = (node) => {
+        if (node == null) return 0
+        let left_value = Math.max(dp(node.left), 0)
+        let right_value = Math.max(dp(node.right), 0)
+        // 总节点之和
+        let price_newpath = node.val + left_value + right_value;
+        // 上次对比求出最大值
+        max = Math.max(max, price_newpath);
+        // 递归获取左右子树的总和
+        return node.val + Math.max(left_value, right_value);
+    }
+    dp(root)
+    return max
 };
