734d5b9ad1220f7784c0266c864eb54fb1211394
YanPing Liu <yanping.liu@questmobile.com.cn>
2019-11-07 19:05:55 +0800

    leetcode

--- leetcode/23. 合并K个排序链表/index.js
+++ leetcode/23. 合并K个排序链表/index.js
@@ -20,3 +20,15 @@ var topKFrequent = function(nums, k) {
     }
     return result
 };
+
+var topKFrequent = function(nums, k) {
+    let map = new Map;
+    for(let i = 0; i < nums.length; i++) {
+        if(map.get(nums[i])) {
+            map.set(nums[i], map.get(nums[i]) + 1);
+        } else {
+            map.set(nums[i], 1);
+        }
+    }
+    return [...map.entries()].sort((a, b) => b[1] - a[1]).slice(0, k).map(v => v[0])
+};
